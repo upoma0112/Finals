ae94dfa72d7465c5fb9a64d0953886089cbb7c72
upoma0112 <su01122022@gmail.com>
2025-01-09 23:15:05 -0800

    10

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ document.getElementById('search-button').addEventListener('click', () => {
       searchMeals(searchText);
     }
   });
+
+  function searchMeals(query) {
+    const mealResultsContainer = document.getElementById('meal-results');
+    const showAllSection = document.getElementById('show-all-section');
+    const showAllButton = document.getElementById('show-all-button');
+    const apiUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`;
+    
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => {
+        const meals = data.meals;
+        mealResultsContainer.innerHTML = '';
